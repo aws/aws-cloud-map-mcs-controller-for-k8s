bfd82eb391a3c125f37f1aa5cefb2e871165d7ce
Dustin Hemmerling <52467318+astaticvoid@users.noreply.github.com>
2021-10-21 12:12:17 -0700

    Refactor client interface for service structs (#46)

---
+++ test/test-constants.go
@@ -0,0 +1,34 @@
+package test
+
+import "github.com/aws/aws-cloud-map-mcs-controller-for-k8s/pkg/model"
+
+const (
+	NsName     = "ns-name"
+	NsId       = "ns-id"
+	SvcName    = "svc-name"
+	SvcId      = "svc-id"
+	EndptId1   = "endpoint-id-1"
+	EndptIp1   = "endpoint-ip-1"
+	EndptPort1 = 2
+	OpId1      = "operation-id-1"
+	OpId2      = "operation-id-2"
+	OpStart    = 1
+)
+
+func GetTestService() *model.Service {
+	endPt := GetTestEndpoint()
+	return &model.Service{
+		Namespace: NsName,
+		Name:      SvcName,
+		Endpoints: []*model.Endpoint{endPt},
+	}
+}
+
+func GetTestEndpoint() *model.Endpoint {
+	return &model.Endpoint{
+		Id:         EndptId1,
+		IP:         EndptIp1,
+		Port:       EndptPort1,
+		Attributes: make(map[string]string, 0),
+	}
+}
