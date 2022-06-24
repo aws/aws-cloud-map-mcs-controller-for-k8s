fdbca2f4b2b4f09c85ee48eeff4de2901a43fcd5
fredjywang <thefblue@gmail.com>
2022-06-24 16:34:11 +0000

    New changes

--- integration/eks-test/scripts/eks-cleanup.sh
+++ integration/eks-test/scripts/eks-cleanup.sh
@@ -7,16 +7,24 @@ source ./integration/eks-test/scripts/eks-common.sh
 # Delete service and namespace from cluster 1 & 2
 $KUBECTL_BIN config use-context $CLUSTER_1
 $KUBECTL_BIN delete svc $SERVICE -n $NAMESPACE
-sleep 30s
 $KUBECTL_BIN delete namespaces $NAMESPACE
-sleep 60s
+eksctl delete iamserviceaccount \
+    --name cloud-map-mcs-controller-manager \
+    --namespace cloud-map-mcs-system \
+    --cluster cls1 \
+    --wait
 
 $KUBECTL_BIN config use-context $CLUSTER_2
 $KUBECTL_BIN delete pod $POD -n $NAMESPACE
 $KUBECTL_BIN delete namespaces $NAMESPACE
+eksctl delete iamserviceaccount \
+    --name cloud-map-mcs-controller-manager \
+    --namespace cloud-map-mcs-system \
+    --cluster cls2 \
+    --wait
 
-# $KUBECTL_BIN config use-context $CLUSTER_1
+$KUBECTL_BIN config use-context $CLUSTER_1
 kubectl delete -k "github.com/aws/aws-cloud-map-mcs-controller-for-k8s/config/controller_install_latest"
-# $KUBECTL_BIN config use-context $CLUSTER_2
+$KUBECTL_BIN config use-context $CLUSTER_2
 kubectl delete -k "github.com/aws/aws-cloud-map-mcs-controller-for-k8s/config/controller_install_latest"
 
--- integration/eks-test/scripts/eks-setup.sh
+++ integration/eks-test/scripts/eks-setup.sh
@@ -2,6 +2,27 @@
 
 source ./integration/eks-test/scripts/eks-common.sh
 
+# Adding IAMService
+$KUBECTL_BIN config use-context $CLUSTER_1
+kubectl create namespace cloud-map-mcs-system
+eksctl create iamserviceaccount \
+--cluster $CLUSTER_1 \
+--namespace cloud-map-mcs-system \
+--name cloud-map-mcs-controller-manager \
+--attach-policy-arn arn:aws:iam::aws:policy/AWSCloudMapFullAccess \
+--override-existing-serviceaccounts \
+--approve
+
+$KUBECTL_BIN config use-context $CLUSTER_2
+kubectl create namespace cloud-map-mcs-system
+eksctl create iamserviceaccount \
+--cluster $CLUSTER_2 \
+--namespace cloud-map-mcs-system \
+--name cloud-map-mcs-controller-manager \
+--attach-policy-arn arn:aws:iam::aws:policy/AWSCloudMapFullAccess \
+--override-existing-serviceaccounts \
+--approve
+
 # Installing controller
 $KUBECTL_BIN config use-context $CLUSTER_1
 kubectl apply -k "github.com/aws/aws-cloud-map-mcs-controller-for-k8s/config/controller_install_latest"
@@ -25,5 +46,5 @@ $KUBECTL_BIN apply -f "$CONFIGS/nginx-serviceexport.yaml"
 # Create client-hello pod
 $KUBECTL_BIN config use-context $CLUSTER_2
 $KUBECTL_BIN apply -f "$CONFIGS/client-hello.yaml"
-sleep 30s
+sleep 15s
 $KUBECTL_BIN exec $POD -n $NAMESPACE /bin/sh -- apk add curl ## install curl
