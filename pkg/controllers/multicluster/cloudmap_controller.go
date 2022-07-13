1ebd09cc33439ba6f00842445095c1cf4ecc85c5
matthewgoodman13 <matthewgoodman13@outlook.com>
2022-07-13 23:08:23 +0000

    cm controller

--- pkg/controllers/multicluster/cloudmap_controller.go
+++ pkg/controllers/multicluster/cloudmap_controller.go
@@ -117,18 +117,6 @@ func (r *CloudMapReconciler) reconcileService(ctx context.Context, svc *model.Se
 
 	importedSvcPorts := ExtractServicePorts(svc.Endpoints)
 
-	r.Log.Debug("service ports", "ports", importedSvcPorts)
-
-	// service ports: {"ports": [{"Name":"","Port":80,"TargetPort":"80","Protocol":"TCP","Attributes":{"CLUSTER_ID":"matthew-cluster-1","K8S_CONTROLLER":"aws-cloud-map-mcs-controller-for-k8s 0.2.3-21-gfe47782-dirty (fe47782-dirty)"}}]}
-
-	// separate importedSvcPorts by unique ClusterID
-	importedSvcPortsByClusterID := make(map[string][]model.Port)
-	for _, p := range importedSvcPorts {
-		importedSvcPortsByClusterID[p.Attributes["CLUSTER_ID"]] = append(importedSvcPortsByClusterID[p.Attributes["CLUSTER_ID"]], *p)
-	}
-
-	r.Log.Debug("service ports by cluster ID", "ports", importedSvcPortsByClusterID)
-
 	svcImport, err := r.getServiceImport(ctx, svc.Namespace, svc.Name)
 	if err != nil {
 		if !errors.IsNotFound(err) {
@@ -139,12 +127,6 @@ func (r *CloudMapReconciler) reconcileService(ctx context.Context, svc *model.Se
 		if svcImport, err = r.createAndGetServiceImport(ctx, svc.Namespace, svc.Name, importedSvcPorts); err != nil {
 			return err
 		}
-	}
-
-	// For each id in importedSvcPortsByClusterID, create derived services
-	for clusterID, importedSvcPortsbyId := range importedSvcPortsByClusterID {
-		r.Log.Debug("der-syncing cluster", "clusterID", clusterID)
-		r.Log.Debug("der-service ports", "ports", importedSvcPortsbyId)
 	}
 
 	derivedService, err := r.getDerivedService(ctx, svc.Namespace, svcImport.Annotations[DerivedServiceAnnotation])
