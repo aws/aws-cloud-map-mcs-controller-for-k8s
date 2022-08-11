b5cdcb13cd0215f0511c0f6947d4d178426a2ec5
Matthew Goodman <matthewgoodman13@outlook.com>
2022-08-11 11:34:49 -0700

    EKS Integration Test Fixes (#187)
    * Added managed by label - issue 110
    
    * derived services + unit tests + label
    
    * revisions and delete svc bug
    
    * annotation readded. kind & janitor test changes.
    
    * fix expected test result. cluster-id -> cluster
    
    * working eks integration tests

--- integration/eks-test/scripts/eks-setup.sh
+++ integration/eks-test/scripts/eks-setup.sh
@@ -2,6 +2,15 @@
 
 source ./integration/eks-test/scripts/eks-common.sh
 
+# Apply ClusterProperties
+$KUBECTL_BIN config use-context $EXPORT_CLS
+make install
+$KUBECTL_BIN apply -f "$CONFIGS/e2e-clusterproperty-1.yaml"
+
+$KUBECTL_BIN config use-context $IMPORT_CLS
+make install
+$KUBECTL_BIN apply -f "$CONFIGS/e2e-clusterproperty-2.yaml"
+
 # Call helper for service account and controller installation
 ./integration/eks-test/scripts/eks-setup-helper.sh $EXPORT_CLS
 ./integration/eks-test/scripts/eks-setup-helper.sh $IMPORT_CLS
@@ -23,4 +32,3 @@ $KUBECTL_BIN apply -f "$CONFIGS/nginx-serviceexport.yaml"
 $KUBECTL_BIN config use-context $IMPORT_CLS
 $KUBECTL_BIN apply -f "$CONFIGS/client-hello.yaml"
 sleep 15
-$KUBECTL_BIN exec $CLIENT_POD -n $NAMESPACE /bin/sh -- apk add curl ## install curl
