2a75e1d19c6cebfebecf7254c50766dee70eedf1
fredjywang <thefblue@gmail.com>
2022-08-16 22:24:14 +0000

    Additional error handling to test

--- integration/kind-test/scripts/run-helper.sh
+++ integration/kind-test/scripts/run-helper.sh
@@ -10,8 +10,8 @@ $KUBECTL_BIN apply -f "$KIND_CONFIGS/e2e-export.yaml"
 ./integration/kind-test/scripts/run-tests.sh "$CLUSTERIP_SERVICE" "ClusterSetIP"
 exit_code=$?
 
+# Headless service test
 if [ "$exit_code" -eq 0 ] ; then
-    # Headless service test
     $KUBECTL_BIN apply -f "$KIND_CONFIGS/e2e-headless.yaml"
     $KUBECTL_BIN apply -f "$KIND_CONFIGS/e2e-headless-export.yaml"
     ./integration/kind-test/scripts/run-tests.sh "$HEADLESS_SERVICE" "Headless"
--- integration/kind-test/scripts/run-tests.sh
+++ integration/kind-test/scripts/run-tests.sh
@@ -33,35 +33,40 @@ fi
 echo "sleeping..."
 sleep 2
 
-deployment=$($KUBECTL_BIN get deployment --namespace "$NAMESPACE" -o json | jq -r '.items[0].metadata.name')
+if [ "$exit_code" -eq 0 ] ; then
+  deployment=$($KUBECTL_BIN get deployment --namespace "$NAMESPACE" -o json | jq -r '.items[0].metadata.name')
 
-echo "scaling the deployment $deployment to $UPDATED_ENDPOINT_COUNT"
-$KUBECTL_BIN scale deployment/"$deployment" --replicas="$UPDATED_ENDPOINT_COUNT" --namespace "$NAMESPACE"
-exit_code=$?
+  echo "scaling the deployment $deployment to $UPDATED_ENDPOINT_COUNT"
+  $KUBECTL_BIN scale deployment/"$deployment" --replicas="$UPDATED_ENDPOINT_COUNT" --namespace "$NAMESPACE"
+  exit_code=$?
+fi
 
 if [ "$exit_code" -eq 0 ] ; then
   if ! updated_endpoints=$(./integration/shared/scripts/poll-endpoints.sh "$UPDATED_ENDPOINT_COUNT") ; then
     exit $?
   fi
+fi
 
+if [ "$exit_code" -eq 0 ] ; then
   go run $SCENARIOS/runner/main.go $NAMESPACE $SERVICE $CLUSTERID1 $CLUSTERSETID1 $ENDPT_PORT $SERVICE_PORT $SERVICE_TYPE "$updated_endpoints"
   exit_code=$?
+fi
 
-  if [ "$exit_code" -eq 0 ] ; then
-    ./integration/shared/scripts/test-import.sh "$UPDATED_ENDPOINT_COUNT" "$updated_endpoints"
-    exit_code=$?
-  fi
+if [ "$exit_code" -eq 0 ] ; then
+  ./integration/shared/scripts/test-import.sh "$UPDATED_ENDPOINT_COUNT" "$updated_endpoints"
+  exit_code=$?
+fi
 
-  if [ "$exit_code" -eq 0 ] ; then
-    ./integration/kind-test/scripts/DNS-test.sh
-    exit_code=$?
-  fi
+if [ "$exit_code" -eq 0 ] ; then
+  ./integration/kind-test/scripts/DNS-test.sh
+  exit_code=$?
 fi
 
 # Scale deployment back down for future test and delete service export
 if [ "$exit_code" -eq 0 ] ; then
   $KUBECTL_BIN scale deployment/"$deployment" --replicas="$EXPECTED_ENDPOINT_COUNT" --namespace "$NAMESPACE"
   $KUBECTL_BIN delete ServiceExport $SERVICE -n $NAMESPACE
+  sleep 5
 fi
 
 echo "killing controller PID:$CTL_PID"
